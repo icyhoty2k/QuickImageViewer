52857d582ca59e59d109a4cbb7f221ba657e2f08
icyhoty2k <icyhoty2k@gmail.com>
2026-06-28 01:42:27 +0300

    update

--- DpiAwareInit.cpp
+++ DpiAwareInit.cpp
@@ -1,26 +1,37 @@
 #include "DpiAwareInit.h"
 #include  "Constants.h"
 
-HWND CreateViewerWindow(HINSTANCE hInstance, const wchar_t* className) {
-    int baseLogicalW = 1200;
-    int baseLogicalH = 800;
+HWND CreateViewerWindow(HINSTANCE hInstance, const wchar_t *className) {
+    // Step 1: Create the window using system DPI for initial placement
+    UINT sysDpi = GetDpiForSystem();
+    int winW = MulDiv(Config::BASE_WIDTH, sysDpi, 96);
+    int winH = MulDiv(Config::BASE_HEIGHT, sysDpi, 96);
 
-    UINT dpi = GetDpiForSystem();
+    int screenW = GetSystemMetricsForDpi(SM_CXSCREEN, sysDpi);
+    int screenH = GetSystemMetricsForDpi(SM_CYSCREEN, sysDpi);
 
-    int winW = MulDiv(baseLogicalW, dpi, 96);
-    int winH = MulDiv(baseLogicalH, dpi, 96);
+    HWND hWnd = CreateWindowExW(
+        WS_EX_APPWINDOW,
+        className, Config::APP_TASKBAR_NAME,
+        WS_POPUP,
+        (screenW - winW) / 2, (screenH - winH) / 2, winW, winH,
+        nullptr, nullptr, hInstance, nullptr
+    );
 
-    int screenW = GetSystemMetricsForDpi(SM_CXSCREEN, dpi);
-    int screenH = GetSystemMetricsForDpi(SM_CYSCREEN, dpi);
+    // Step 2: Now that the window exists, get the actual monitor DPI it landed on
+    UINT actualDpi = GetDpiForWindow(hWnd);
+    if (actualDpi != sysDpi) {
+        int actualW = MulDiv(Config::BASE_WIDTH, actualDpi, 96);
+        int actualH = MulDiv(Config::BASE_HEIGHT, actualDpi, 96);
 
-    int posX = (screenW - winW) / 2;
-    int posY = (screenH - winH) / 2;
+        MONITORINFO mi = {sizeof(mi)};
+        GetMonitorInfo(MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST), &mi);
 
-    return CreateWindowExW(
-           WS_EX_APPWINDOW,
-           className, Config::APP_TASKBAR_NAME,
-           WS_POPUP,
-           posX, posY, winW, winH,
-           nullptr, nullptr, hInstance, nullptr
-       );
+        int posX = mi.rcMonitor.left + (mi.rcMonitor.right - mi.rcMonitor.left - actualW) / 2;
+        int posY = mi.rcMonitor.top + (mi.rcMonitor.bottom - mi.rcMonitor.top - actualH) / 2;
+
+        SetWindowPos(hWnd, nullptr, posX, posY, actualW, actualH, SWP_NOZORDER | SWP_NOACTIVATE);
+    }
+
+    return hWnd;
 }
