091d29609e47ffaa6463c433f2c7cc5e8efed7d0
icyhoty2k <icyhoty2k@gmail.com>
2026-07-08 18:03:07 +0300

    Fix stacked layout: left-align all slots, correct order,  effects last, restore fmt on exit

---
+++ src/Platform/ConstantsStrings.h
@@ -0,0 +1,6 @@
+//Made just for ease of management and internationalization.
+// This file will contain all messages for the center center overlay !
+namespace Constants::Messages {}
+
+//This will contain all strings other than messages for center center overlay because if app is internationalized i need to store the defaults
+namespace Constants::Strings {}
