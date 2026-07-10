ff6c234830c5e8efc9a78b2d227f1695cc8ef14c
icyhoty2k <icyhoty2k@gmail.com>
2026-07-11 00:58:32 +0300

    Improve thumbnail loading logic to handle folders and files.

--- src/Platform/Constants.h
+++ src/Platform/Constants.h
@@ -214,8 +214,9 @@ namespace Constants {
         // Folder History (HistoryWindow)
         // =========================================================================
         constexpr const wchar_t *HISTORY_FILE_NAME = L"qivHistory.txt";
+        //theese are kept im mot recently used order in ram , when addin a new one to file just append to end with no duplicates
         constexpr int HISTORY_MAX_DIRS_TO_SHOW = 10; // how many folders to show in historyWnd
-        constexpr int HISTORY_MAX_DIRS_TO_SAVE = 1000; // how many folders to remember/sava in file
+        constexpr int HISTORY_MAX_DIRS_TO_SAVE = 1000; // how many folders to remember/sava in file , just append to end new ones until max is reached excluding duplicates
         constexpr char HISTORY_FAVORITES_MARK = '*'; // mark for favorites appened before the file name
         constexpr int HISTORY_MAX_FAVORITES_TO_SHOW = 10; // how many favorites folders to show in HistoryWnd
         constexpr int HISTORY_FAVORITES_POSITION = 0; // 0 on top , 1 on bottom , 2 don't change position(not pinned)
