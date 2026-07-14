e3b2eb4dba91dd2da907a75e144097f06890d6a6
icyhoty2k <icyhoty2k@gmail.com>
2026-07-14 21:23:23 +0300

    Fix stacked layout: left-align all slots, correct order,  effects last, restore fmt on exit

--- src/UI/UIManager.h
+++ src/UI/UIManager.h
@@ -176,6 +176,7 @@ namespace UI {
             void             ToggleJumpToWindow();   // hides FindWnd if visible, then toggles JumpToWnd
             void             ToggleFindWindow();     // hides JumpToWnd if visible, then toggles FindWnd
             StatsWnd        &getStatsWindow();
+            void             ApplyAlwaysOnTop(bool onTop);
 
             void SpawnDirWndForFolder(const std::wstring &folderPath, HWND hHistoryWnd);
             void HideAllSpawnedDirWnds();
