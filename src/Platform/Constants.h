7e5c8ac338d43a108777736c085dd6b494f775ef
icyhoty2k <icyhoty2k@gmail.com>
2026-07-17 07:46:08 +0300

    version bump

--- src/Platform/Constants.h
+++ src/Platform/Constants.h
@@ -7,8 +7,8 @@
 // =========================================================================
 #define VER_MAJOR 2
 #define VER_MINOR 4
-#define VER_PATCH 0
-#define VER_BUILD 0
+#define VER_PATCH 1
+#define VER_BUILD 17072026
 
 // Comma form  — FILEVERSION / PRODUCTVERSION in .rc  (e.g. 2,3,0,0)
 #define VER_NUMERIC   VER_MAJOR,VER_MINOR,VER_PATCH,VER_BUILD
