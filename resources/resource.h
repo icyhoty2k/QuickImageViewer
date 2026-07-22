49b305c0eb200fe1b9253edd2c5870fa3a86402b
icyhoty2k <icyhoty2k@gmail.com>
2026-07-23 01:36:47 +0300

    chore: update resource paths and menu order, bump version patch

--- resources/resource.h
+++ resources/resource.h
@@ -1,7 +1,12 @@
 #pragma once
+//Icons
 #define IDI_APP_ICON           101
 #define IDI_APP_ICON_DEDICATED 102
+//GeoData
 #define IDR_CITIES_DATA        103
 #define IDR_ADMIN1_DATA        104
 #define IDR_ADMIN2_DATA        105
 #define IDR_COUNTRY_DATA       106
+// Cursors
+#define IDC_ZOOM_CURSOR        107
+#define IDC_GRAB_CURSOR        108
