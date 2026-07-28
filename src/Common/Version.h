2e62ff11c15a3b64e7ca46223d496fa8f3175795
icyhoty2k <icyhoty2k@gmail.com>
2026-07-29 00:28:31 +0300

    chore(version): update version to 96.0.59
    1. - Prepare for the next release cycle.
    2. - Align build number with new minor version.
    
    - Update VER_MINOR in Version.h from 90 to 96.
    - Increment BuildNumber.txt from 57 to 59.
    
    Refs: Version.h, BuildNumber.txt

--- src/Common/Version.h
+++ src/Common/Version.h
@@ -31,7 +31,7 @@
 // Used by the Resource Compiler for version metadata.
 // =========================================================================
 #define VER_MAJOR 2
-#define VER_MINOR 90
+#define VER_MINOR 96
 #define VER_PATCH 0
 
 #define FILE_DESC     "qIV"
