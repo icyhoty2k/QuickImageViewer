d00235fa7e79675f92bec777249a5a164a52be87
Ivan Hristov Yanev <icyhoty2k@gmail.com>
2026-08-14 07:26:26 +0300

    chore(version): update version and build number
    1. - Prepare for the next release cycle.
    2. - Ensure consistency between Version.h and BuildNumber.txt.
    
    - Update VER_MINOR from 252 to 263 in Version.h.
    - Update build number from 331 to 333 in BuildNumber.txt.
    
    Refs: Version.h, BuildNumber.txt
    Signed-off-by: Ivan Hristov Yanev <icyhoty2k@gmail.com>

--- src/Common/Version.h
+++ src/Common/Version.h
@@ -39,7 +39,7 @@
 // Used by the Resource Compiler for version metadata.
 // =========================================================================
 #define VER_MAJOR 2
-#define VER_MINOR 252
+#define VER_MINOR 263
 #define VER_PATCH 0
 
 #define FILE_DESC     "qIV"
